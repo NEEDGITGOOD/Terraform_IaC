8ae33257db070a2407c8bd410206bff3c9f76444
NEEDGITGOOD <theclowner111@gmail.com>
2023-12-11 12:51:35 +0000

    Fix bug in login functionality

---
+++ Digital_Ocean/Docker/main.tf
@@ -0,0 +1,30 @@
+resource "digitalocean_droplet" "Docker-VM01" {
+  image = "docker-20-04"
+  name = "Docker-VM01"
+  region = "fra1"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+
+# Connect and Install Updates
+connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "4m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      "sudo apt update",
+      "sudo docker run -d -p 80:80 nginx"
+    ]
+  }
+}
+
+output "public_ip" {
+  value = digitalocean_droplet.Docker-VM01.ipv4_address
+  description = "The public IP address of the droplet!"
+}
