5dcf8f22bcf7957f52ea25eb3ac0c8f625d9723c
NEEDGITGOOD <theclowner111@gmail.com>
2023-12-22 23:35:32 +0000

    Update docker-compose.yaml and output.tf

--- Digital_Ocean/Docker/ProvisionTest/output.tf
+++ Digital_Ocean/Docker/ProvisionTest/output.tf
@@ -2,3 +2,18 @@ output "public_ip" {
   value = digitalocean_droplet.Docker01.ipv4_address
   description = "The public IP address of the droplet!"
 }
+
+output "public_ip" {
+  value = digitalocean_droplet.Docker01.ipv4_address_private
+  description = "The public IP address of the droplet!"
+}
+
+output "public_ip" {
+  value = digitalocean_droplet.Docker02.ipv4_address
+  description = "The public IP address of the droplet!"
+}
+
+output "public_ip" {
+  value = digitalocean_droplet.Docker02.ipv4_address_private
+  description = "The public IP address of the droplet!"
+}
