add06b611c6c76cbf06bddfd91b8983b701ac891
NEEDGITGOOD <theclowner111@gmail.com>
2023-12-31 16:55:37 +0000

    Update cloud-init and docker-compose files
    Stop and disable systemd-resolved service
    
    Remove port mapping for port 80 in docker-compose file
    
    Update URLs in dashy_template.yml

--- Digital_Ocean/NetBox/main.tf
+++ Digital_Ocean/NetBox/main.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "netbox" {
   image = "netverity-netbox-20-04"
   name = "VPN01"
   region = "fra1"
-  size = "s-1vcpu-1gb"
+  size = "s-1vcpu-2gb"
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
