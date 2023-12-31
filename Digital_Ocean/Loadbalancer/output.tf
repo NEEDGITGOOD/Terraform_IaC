804782d381a3cf23791e3e8c259aac3b260ffe8f
NEEDGITGOOD <theclowner111@gmail.com>
2023-12-31 05:10:21 +0000

    Remove load balancer configuration and update output.tf

--- Digital_Ocean/Loadbalancer/output.tf
+++ Digital_Ocean/Loadbalancer/output.tf
@@ -1,17 +1,4 @@
-output "public_ip_ww-1" {
-  value = digitalocean_droplet.www-1.ipv4_address
-  description = "Nginx Webserver 01"
+output "public_ips_www" {
+  value = { for i, droplet in digitalocean_droplet.www : "www-${i+1}" => droplet.ipv4_address }
+  description = "IP Addresses of Nginx Webservers"
 }
-
-output "public_ip_ww-2" {
-  value = digitalocean_droplet.www-2.ipv4_address
-  description = "Nginx Webserver 02"
-}
-
-#### Docker02 ####
-
-output "public_ip_Docker02" {
-  value = digitalocean_droplet.www-lb.ipv4_address
-  description = "Loadbalancer"
-}
-
