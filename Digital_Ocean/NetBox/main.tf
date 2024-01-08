b50613edfee13da66e7105f9447de22579dfb65e
NEEDGITGOOD <theclowner111@gmail.com>
2024-01-08 15:06:34 +0100

    add firewall to netbox

--- Digital_Ocean/NetBox/main.tf
+++ Digital_Ocean/NetBox/main.tf
@@ -7,3 +7,48 @@ resource "digitalocean_droplet" "netbox" {
     data.digitalocean_ssh_key.terraform.id
   ]
 }
+
+resource "digitalocean_firewall" "netbox_firewall" {
+  name = "netbox-firewall"
+
+  droplet_ids = [ 
+    digitalocean_droplet.netbox.id,
+  ]
+
+ ## Inbound Rules
+
+  ### Allow Inbound HTTPS (443)
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["207.154.228.93"]
+  }
+
+  ### Allow Inbound ICMP
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["207.154.228.93"]
+  }
+
+ ## Outbound Rules (allow to any)
+
+  ### Allow Outbound TCP (1-65535)
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  ### Allow Outbound UDP (1-65535)
+  outbound_rule {
+    protocol         = "udp"
+    port_range       = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  ### Allow Outbound ICMP
+  outbound_rule {
+    protocol         = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
