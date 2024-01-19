06cea52de3dc51d74830774bf9ca459991ab93ff
NEEDGITGOOD <theclowner111@gmail.com>
2024-01-19 13:41:59 +0100

    revert

--- Digital_Ocean/Docker/ProvisionTest_Cloud_Init/DockerVMs_CloudInitVariant.tf
+++ Digital_Ocean/Docker/ProvisionTest_Cloud_Init/DockerVMs_CloudInitVariant.tf
@@ -1,9 +1,8 @@
-### Portainer Admin pw
-
-variable "portainer_password" {
-  description = "Portainer Admin Passwort"
-  type        = string
-}
+### portainer pw
+#variable "portainer_password" {
+#  description = "Portainer Admin Passwort"
+#  type        = string
+#}
 
 ### Alma Linux
 
@@ -211,7 +210,7 @@ resource "digitalocean_droplet" "Docker01" {
       "bash setupDocker01IP.sh", # Run the Script, which changes the Docker01 Placeholder in the Gatos Config file to the Docker01 IP.
       "chmod 600 ~/.ssh/id_rsa", # Change the permissions of the ssh file
       "sudo docker compose -f /root/docker-compose.yaml up -d", # Run the docker-compose file
-      "http POST localhost:9000/api/users/admin/init Username='admin' Password='${var.portainer_password}'"
+      "http POST localhost:9000/api/users/admin/init Username=\"admin\" Password=\"admin01admin01\"" # Create the admin user
     ]
   }
 
