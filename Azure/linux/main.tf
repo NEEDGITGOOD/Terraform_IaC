1e569ffe1104db26748e8807577b3945eb494d78
NEEDGITGOOD <theclowner111@gmail.com>
2024-01-22 18:30:42 +0100

    .

--- Azure/linux/main.tf
+++ Azure/linux/main.tf
@@ -3,40 +3,40 @@ resource "azurerm_resource_group" "example" {
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "ter" "vnet" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "sn" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "nic" {
   name                = "example-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.sn.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
+resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.nic.id,
   ]
 
   os_disk {
@@ -51,3 +51,33 @@ resource "azurerm_linux_virtual_machine" "example" {
     version   = "latest"
   }
 }
+
+resource "azurestack_virtual_machine" "terraform-vm1" {
+    name                  = "terraform-vm1"
+    location              = azurestack_resource_group.deployment.location
+    resource_group_name   = azurestack_resource_group.deployment.name
+    network_interface_ids = [
+        azurestack_network_interface.terraform-vm1-nic.id
+        ]
+    vm_size               = "Standard_F2"
+  
+    storage_image_reference {
+      publisher = "Canonical"
+      offer     = "UbuntuServer"
+      sku       = "18.04-LTS"
+      version   = "latest"
+    }
+    storage_os_disk {
+      name              = "terraform-vm1-osdisk"
+      create_option     = "FromImage"
+      managed_disk_type = "Standard_LRS"
+    }
+    os_profile {
+      computer_name  = "hostname"
+      admin_username = "testadmin"
+      admin_password = "Password1234!"
+    }
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+  }
