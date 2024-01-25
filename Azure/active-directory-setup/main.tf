f595bb18dd0d10c7f54f6322e82cbf5eb34aaf8a
NEEDGITGOOD <theclowner111@gmail.com>
2024-01-25 18:14:11 +0100

    fix variable

--- Azure/active-directory-setup/main.tf
+++ Azure/active-directory-setup/main.tf
@@ -12,8 +12,7 @@ module "network" {
   
   resource_group_name = module.resource_group.resource_group_name
   location  = var.location
-  adds_domain_controller_ip_addresses = var.active-directory.adds_domain_controller_ip_addresses
-
+  adds_domain_controller_ip_addresses = module.active-directory.adds_domain_controller_ip_addresses
 }
 
 module "windows_vm" {
