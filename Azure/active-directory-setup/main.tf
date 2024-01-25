b7dce75a069030bdbcb72c7312c83fe8f10ca0c0
NEEDGITGOOD <theclowner111@gmail.com>
2024-01-25 18:24:49 +0100

    x

--- Azure/active-directory-setup/main.tf
+++ Azure/active-directory-setup/main.tf
@@ -12,7 +12,7 @@ module "network" {
   
   resource_group_name = module.resource_group.resource_group_name
   location  = var.location
-  adds_domain_controller_ip_addresses = module.active-directory.adds_domain_controller_ip_addresses
+  dns_servers = module.active-directory.adds_domain_controller_ip_addresses
 }
 
 module "windows_vm" {
