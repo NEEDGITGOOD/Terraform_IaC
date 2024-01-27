862ec05b0711ed2b4bbf4c0bb6006ba80613b6fa
NEEDGITGOOD <theclowner111@gmail.com>
2024-01-27 21:43:31 +0100

    restructure nsg rules

--- Azure/active-directory-setup/modules/network/main.tf
+++ Azure/active-directory-setup/modules/network/main.tf
@@ -1,20 +1,3 @@
-# Create Vnet for AADS
-resource "azurerm_virtual_network" "vnet2" {
-  name                = "${var.resource_group_name}-VNET02"
-  address_space       = ["10.10.20.0/24"]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-}
-
-# Create Subnet
-resource "azurerm_subnet" "subnet2" {
-  name                 = "${var.resource_group_name}-SN02"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet2.name
-  address_prefixes     = ["10.10.20.0/25"]
-}
-
 # Create Vnet
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.resource_group_name}-VNET01"
@@ -25,8 +8,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 }
 
-
-# Create Subnet
+## Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.resource_group_name}-SN01"
   resource_group_name  = var.resource_group_name
@@ -34,8 +16,24 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.10.10.0/25"]
 }
 
-# Create Security Group (Firewall) (for Windows Machines)
+# Create Vnet for AADS
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "${var.resource_group_name}-VNET02"
+  address_space       = ["10.10.20.0/24"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
+}
+
+## Create Subnet
+resource "azurerm_subnet" "subnet2" {
+  name                 = "${var.resource_group_name}-SN02"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes     = ["10.10.20.0/25"]
+}
+
+# Create Security Group (Firewall) (for Windows Machines)
 resource "azurerm_network_security_group" "nsg01" {
   name                = "${var.resource_group_name}-NSG01"
   location            = var.location
@@ -80,7 +78,7 @@ resource "azurerm_network_security_group" "nsg02" {
     protocol                   = "TCP"
     source_port_range          = "*"
     destination_port_range     = "3389"
-    source_address_prefix      = "207.154.228.93"
+    source_address_prefix      = "10.10.10.0/24"
     destination_address_prefix = "*"
   }
 
@@ -116,6 +114,19 @@ resource "azurerm_network_security_group" "nsg02" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "636"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  
+  security_rule {
+    name                       = "AllowDNS"
+    priority                   = 501
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "53"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
--- Azure/active-directory-setup/modules/windows-vms/main.tf
+++ Azure/active-directory-setup/modules/windows-vms/main.tf
@@ -12,12 +12,63 @@ resource "azurerm_network_interface" "ni" {
 
 }
 
-resource "azurerm_network_interface_security_group_association" "example" {
+resource "azurerm_network_interface_security_group_association" "ni_nsg01" {
   network_interface_id      = azurerm_network_interface.ni.id
   network_security_group_id = var.nsg_id
 }
 
 resource "azurerm_public_ip" "vm_pip" {
+  name                = "${var.vm_name}-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.ni.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "win10-22h2-pro"
+    version   = "latest"
+  }
+}
+
+
+### Second Windows Client
+
+resource "azurerm_network_interface" "ni02" {
+  name                = "${var.vm_name}-ni02"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal02"
+    subnet_id                          = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_pip.id
+  }
+
+}
+
+resource "azurerm_network_interface_security_group_association" "ni_nsg02" {
+  network_interface_id      = azurerm_network_interface.ni.id
+  network_security_group_id = var.nsg_id
+}
+
+resource "azurerm_public_ip" "vm_pip02" {
   name                = "${var.vm_name}-pip"
   location            = var.location
   resource_group_name = var.resource_group_name
