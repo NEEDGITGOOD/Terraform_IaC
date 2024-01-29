14ac9e122742135cab35272879a9683e12916e66
NEEDGITGOOD <theclowner111@gmail.com>
2024-01-29 08:51:42 +0100

    Fix Docker IPs

--- Digital_Ocean/Docker/ProvisionTest_Cloud_Init/DockerVMs_CloudInitVariant.tf
+++ Digital_Ocean/Docker/ProvisionTest_Cloud_Init/DockerVMs_CloudInitVariant.tf
@@ -239,6 +239,60 @@ resource "digitalocean_droplet" "Docker01" {
   ]
 }
 
+# Firewall for Netbox01
+resource "digitalocean_firewall" "docker01_firewall" {
+  name = "docker01-firewall"
+
+  droplet_ids = [ 
+    digitalocean_droplet.Docker01.id,
+  ]
+
+ ## Inbound Rules
+
+  ### Allow Inbound HTTPS (443)
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["207.154.228.93"]
+  }
+
+  ### Allow Inbound ICMP
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["207.154.228.93"]
+  }
+
+  ### Allow Inbound ssh from jumpbox
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["206.81.16.20"]
+  }
+
+ ## Outbound Rules (allow to any)
+
+  ### Allow Outbound TCP (1-65535)
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  ### Allow Outbound UDP (1-65535)
+  outbound_rule {
+    protocol         = "udp"
+    port_range       = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  ### Allow Outbound ICMP
+  outbound_rule {
+    protocol         = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+
 # SSH Tunnel Script Creator
 resource "null_resource" "setup_ssh_tunnels" {
 
