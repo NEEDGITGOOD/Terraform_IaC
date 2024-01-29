ce21abba948f72b18f5ad4d04f83ac1b6d806057
NEEDGITGOOD <theclowner111@gmail.com>
2024-01-29 13:22:13 +0100

    refactor win/linux

--- Azure/active-directory-setup/main.tf
+++ Azure/active-directory-setup/main.tf
@@ -20,7 +20,7 @@ module "windows_vm" {
   vm_size             = "Standard_DS1_v2"
   admin_username      = var.windows_username
   admin_password      = var.windows_password
-  nsg_id01    = module.network.nsg_id01
+  nsg_id01            = module.network.nsg_id01
 }
 
 # Import Active Directory Module
@@ -32,5 +32,14 @@ module "active-directory" {
   adds_domain_name    = var.adds_domain_name
   adds_admin_username = var.adds_admin_username
   adds_admin_password = var.adds_admin_password
+  nsg_id    = module.network.nsg_id02
+}
+
+# Import Linux Module
+module "linux-vm" {
+  source    = "./modules/linux-vm"
+  subnet_id = module.network.subnet_id
+  resource_group_name = module.resource_group.resource_group_name
+  location            = var.location
   nsg_id    = module.network.nsg_id02
 }
--- Azure/active-directory-setup/modules/linux-vm/main.tf
+++ Azure/active-directory-setup/modules/linux-vm/main.tf
@@ -1,40 +1,40 @@
 ## Network Interface Card 01
-resource "azurerm_network_interface" "ni01" {
-  name                = "${var.vm_name}-ni01"
+resource "azurerm_network_interface" "ni04" {
+  name                = "${var.vm_name}-ni04"
   location            = var.location
   resource_group_name = var.resource_group_name
 
   ip_configuration {
-    name                          = "internal01"
+    name                          = "internal04"
     subnet_id                          = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_pip01.id
+    public_ip_address_id          = azurerm_public_ip.vm_pip04.id
   }
 }
 
 ## Associate NIC to Public IP 01
-resource "azurerm_network_interface_security_group_association" "ni_nsg01" {
-  network_interface_id      = azurerm_network_interface.ni01.id
+resource "azurerm_network_interface_security_group_association" "ni_nsg04" {
+  network_interface_id      = azurerm_network_interface.ni04.id
   network_security_group_id = var.nsg_id01
 }
 
 ## Create Public IP 01 (Windows Client01 - Win10)
-resource "azurerm_public_ip" "vm_pip01" {
-  name                = "${var.vm_name}-pip01"
+resource "azurerm_public_ip" "vm_pip04" {
+  name                = "${var.vm_name}-pip04"
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
 }
 
-### Create Windows VM 01
-resource "azurerm_windows_virtual_machine" "vm01" {
+### Create Windows VM 04
+resource "azurerm_windows_virtual_machine" "vm04" {
   name                = "${var.vm_name}-Win10"
   resource_group_name = var.resource_group_name
   location            = var.location
   size                = var.vm_size
   admin_username      = var.admin_username
   admin_password      = var.admin_password
-  network_interface_ids = [azurerm_network_interface.ni01.id]
+  network_interface_ids = [azurerm_network_interface.ni04.id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -42,55 +42,9 @@ resource "azurerm_windows_virtual_machine" "vm01" {
   }
 
   source_image_reference {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-10"
-    sku       = "win10-22h2-pro"
-    version   = "latest"
-  }
-}
-
-### Create Windows VM 02
-resource "azurerm_windows_virtual_machine" "vm02" {
-  name                = "${var.vm_name}-Win11"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  network_interface_ids = [azurerm_network_interface.ni02.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-11"
-    sku       = "win11-22h2-pro"
-    version   = "latest"
-  }
-}
-
-### Create Windows Server VM03
-resource "azurerm_windows_virtual_machine" "vm03" {
-  name                = "WinServer01"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  network_interface_ids = [azurerm_network_interface.ni03.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
